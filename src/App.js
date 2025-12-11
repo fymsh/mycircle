cc679fa20ad9c74b396867c2ab1fde58cc46c0c0
fymsh <fahimbovak@gmail.com>
2025-12-11 23:33:13 +0800

    rename brand

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
     return (
       <div className="loading-screen">
         <div className="loading-spinner"></div>
-        <div className="loading-text">Loading Nexus</div>
+        <div className="loading-text">Loading</div>
         <div className="loading-subtext">Connecting to your circle...</div>
       </div>
     );
